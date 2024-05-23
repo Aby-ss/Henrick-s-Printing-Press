c81af7aa5f6354c7af413ff0df333ff20334edb4
Aby-ss <raoabdulhadi952@gmail.com>
2024-05-24 01:43:38 +0400

    Animation Integration #2

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 import { Application } from "https://cdn.skypack.dev/@splinetool/runtime@0.9.416";
 
-const canvas = document.getElementById('canvas3d');
+const canvas1 = document.getElementById('product');
+const app1 = new Application(canvas1);
+app1.load('https://prod.spline.design/NSAjxdz9NJzZ8sZi/scene.splinecode');
 
-const app = new Application(canvas);
+const canvas2 = document.getElementById('box');
+const app2 = new Application(canvas2);
+app2.load('https://prod.spline.design/YmD5GmNpdV1D0Ghg/scene.splinecode');
 
-app.load('https://prod.spline.design/NSAjxdz9NJzZ8sZi/scene.splinecode');
